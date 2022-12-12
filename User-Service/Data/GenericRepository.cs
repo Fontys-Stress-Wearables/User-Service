137949b7486f63f21a43ed222d76a2690d13f81f
Milan <58393068+Desoxyr@users.noreply.github.com>
2022-12-12 13:15:16 +0100

    Update naming convention in GenericRepository.cs

--- User-Service/Data/GenericRepository.cs
+++ User-Service/Data/GenericRepository.cs
@@ -4,36 +4,36 @@ namespace User_Service.Data
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        protected readonly DatabaseContext _context;
+        protected readonly DatabaseContext Context;
 
         public GenericRepository(DatabaseContext context)
         {
-            _context = context;
+            Context = context;
         }
 
         public T Add(T entity)
         {
-            return _context.Set<T>().Add(entity).Entity;
+            return Context.Set<T>().Add(entity).Entity;
         }
 
         public IEnumerable<T> GetAll()
         {
-            return _context.Set<T>().ToList();
+            return Context.Set<T>().ToList();
         }
 
         public T? GetById(string id)
         {
-            return _context.Set<T>().Find(id);
+            return Context.Set<T>().Find(id);
         }
 
         public T Update(T entity)
         {
-            return _context.Set<T>().Update(entity).Entity;
+            return Context.Set<T>().Update(entity).Entity;
         }
 
         public void Remove(T entity)
         {
-            _context.Set<T>().Remove(entity);
+            Context.Set<T>().Remove(entity);
         }
     }
 }
