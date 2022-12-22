306f0b495e3efae4002ad4f724658ef70ae08efc
Michael Osuntuyi <mikosuntuyi@gmail.com>
2022-12-22 11:42:24 +0100

    CHanges to merge conflicts fix

--- User-Service/Data/PatientGroupRepository.cs
+++ User-Service/Data/PatientGroupRepository.cs
@@ -1,4 +1,5 @@
-﻿using User_Service.Interfaces.IRepositories;
+﻿using Microsoft.EntityFrameworkCore;
+using User_Service.Interfaces.IRepositories;
 using User_Service.Models;
 
 namespace User_Service.Data
@@ -10,27 +11,29 @@ namespace User_Service.Data
 
         }
 
+        // return all patient groups in this organisation
         public IEnumerable<PatientGroup> GetAllFromTenant(string tenantId)
         {
-            return Context.Set<PatientGroup>().Where(x => x.Organisation.Id == tenantId).ToList();
+            return _context.Set<PatientGroup>().Where(x => x.Organisation.Id == tenantId).ToList();
         }
 
         public void AddUser(PatientGroup patientGroup, User user)
         {
             patientGroup.Users.Add(user);
 
-            Context.PatientGroups.Update(patientGroup);
+            _context.PatientGroups.Update(patientGroup);
         }
-        
+
+
         public PatientGroup? GetByIdAndTenant(string id, string tenantId)
         {
-            return Context.Set<PatientGroup>().Where(x => x.Organisation.Id == tenantId).FirstOrDefault(x => x.Id == id);
+            return _context.Set<PatientGroup>().Where(x => x.Organisation.Id == tenantId).FirstOrDefault(x => x.Id == id);
         }
 
         public void RemoveUser(PatientGroup patientGroup, User user)
         {
             patientGroup.Users.Remove(user);
-            Context.Update(patientGroup);
+            _context.Update(patientGroup);
         }
     }
 }
